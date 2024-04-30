650959e8d33b142843e20e4745ce0bb6377b7dde
Jhonata anderson <jhonatacursos543@gmail.com>
2024-04-30 00:30:43 -0300

    primeiro commit

---
+++ Nível 1/calculadora.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+
+print("""
+Resultado da adição: {}
+Resultado da subtração: {}
+Resultado da multiplicação: {}
+Resultado da divisão: {}
+""" .format(n1+n2, n1-n2, n1*n2, n1/n2))
